0ad8aaa8fc0fdb4f678b068b5bad86c8de668e65
Burak Ünal <55330282+BurakUnal34@users.noreply.github.com>
2023-06-15 17:40:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var slides = document.querySelectorAll(".slide");
+    var currentSlide = 0;
+  
+    function showSlide(n) {
+      slides[currentSlide].classList.remove("active");
+      currentSlide = (n + slides.length) % slides.length;
+      slides[currentSlide].classList.add("active");
+    }
+  
+    function changeSlide(n) {
+      showSlide(currentSlide + n);
+    }
+  
+    document.querySelector(".prev").addEventListener("click", function() {
+      changeSlide(-1);
+    });
+  
+    document.querySelector(".next").addEventListener("click", function() {
+      changeSlide(1);
+    });
+  
+    showSlide(currentSlide);
+  });
